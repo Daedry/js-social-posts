7cd5c75649c4bd70306c45203d01425a8809b54e
MyUsername <romamargine@gmail.com>
2022-04-12 18:30:11 +0200

    add some fix stuff

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,7 +14,7 @@ Non è necessario creare date casuali
 const post = [
   {
     id: 1,
-    description: "La via del saggio è agire, ma non competere.",
+    description: "La via del saggio è agire, ma non competere. <br> Quando sei contento di essere semplicemente te stesso e non fai confronti e non competi, tutti ti rispetteranno.",
     image:
       "https://www.revistaesfinge.com/wp-content/uploads/2016/01/ae4bbf432d7a347a7d7b6f051b071f94.jpg",
     avatar:
@@ -26,7 +26,7 @@ const post = [
 
   {
     id: 2,
-    description: "Io e il mio amico del cuore Kurama ",
+    description: "“La morte è l’unica saggia consigliera che abbiamo. Ogni volta che senti, come fai sempre, che tutto va male e stai per essere annientato, rivolgiti alla tua morte e chiedile se è proprio così. Lei ti dirà che stai sbagliando , che in realtà niente conta al di fuori del suo tocco. La morte ti dirà “non ti ho ancora toccato”.",
     image:
       "https://www.coachingmagnetico.it/wp-content/uploads/2017/10/37036a882618b60477afb2969f8289b5-d5oaglj.jpg",
     avatar:
@@ -39,7 +39,7 @@ const post = [
   {
     id: 3,
     description:
-      "Movimenti e Danze Sacre di Gurdjieff per lo sviluppo armonico dell’Uomo",
+      "Movimenti e Danze Sacre di Gurdjieff per lo sviluppo armonico dell’Uomo <br> I Movimenti e le Danze Sacre sono un’antica disciplina tramandata per aiutare l’essere umano a contattare ed esplorare il proprio mondo interiore. Sono un insieme di movimenti differenti: figure geometriche, preghiere, danze femminili e rituali maschili provenienti dalla tradizione derviscia. La musica, creata da Gurdjieff è ricca di armonia, melodia e ritmo. Questi movimenti producono un cambiamento nel corpo, nella circolazione del sangue, nel ritmo respiratorio, portando uno stato di lucidità e di presenza cosciente.",
     image:
       "https://www.supernaturalcafe.it/wp-content/uploads/2016/01/gurdjieff1.jpg",
     avatar: "https://qcgonv.files.wordpress.com/2015/10/gurdjieff.jpeg",
